5a173d9f1737ec8d32a6ddb3871b7e0c58a22c2f
bb3ry <bbambery@gmail.com>
2022-08-23 04:46:15 -0400

    More CSS

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -29,9 +29,14 @@ const Schedule = ({ heading }) => {
                         <FullTime>
                             <GiGolfTee color="#C63893" />   {item.node.startTime}
                         </FullTime>
-                        <Task>
-                            {item.node.event}
-                        </Task>
+                        <Event>
+                            <Task>
+                                {item.node.event}
+                            </Task>
+                            <Desc>
+                                {item.node.eventDescription}
+                            </Desc>
+                        </Event>
                     </ScheduleFrame>
                 </ListItem>
             )
@@ -50,14 +55,22 @@ const Schedule = ({ heading }) => {
 export default Schedule
 
 const ScheduleContainer = styled.div`
-border-radius: 4px;
-border-bottom: 3px solid darken(#2D3134, 15%);
-margin: 0 auto;
-padding-top: 1rem;
-width: 90%;
-margin-top: 1.5rem;
-margin-bottom: 20px;
-max-width:100%;
+    border-radius: 4px;
+    border-bottom: 3px solid darken(#2D3134, 15%);
+    margin: 0 auto;
+    padding-top: 1rem;
+    width: 90%;
+    margin-top: 1.5rem;
+    margin-bottom: 20px;
+    max-width:100%;
+    padding-left:5rem;
+    padding-right:5rem;
+    min-width:250px;
+    @media screen and (max-width:1024px){
+        padding-left:1rem;
+        padding-right:1rem;
+    }
+
 `
 const ScheduleHeading = styled.h1`
      color: #6660A9;
@@ -70,25 +83,51 @@ const ScheduleList = styled.ul`
 const ScheduleFrame = styled.div`
     display: table;
     width: 100%;
+    height:1rem;
 `
 
 const FullTime = styled.div`
-display: table-cell;
-min-width: 75px;
+    display: table-cell;
+    min-width: 75px;
     padding: 0 20px 0 10px;
     text-align: left;
     align-items: center;
     font-weight: 700;
+    font-size: 1rem;
+    @media screen and (max-width:400px){
+        font-size: 0.9rem;
+    }
 `
-const Task = styled.div`
-font-size: 18px;
-    padding: 0 10px 0 10px;
+const Event = styled.div`
+    color:#ad3181;
     text-align: right;
     font-weight: 700;
 `
+const Desc = styled.div`
+    display:block;
+    font-size: 0.8rem;
+    color: #6660A9;
+    text-align: right;
+    font-weight: 700;
+    padding: 0 10px;
+    @media screen and (max-width:400px){
+        font-size: 0.7rem;
+        padding: 0;
+    }
+`
+const Task = styled.div`
+    font-size: 1rem;
+    padding: 0 10px;
+    text-align: right;
+    font-weight: 700;
+    @media screen and (max-width:400px){
+        font-size: 0.9rem;
+        padding: 0;
+    }
+`
 const ListItem = styled.li`
-padding: 10px;
-border-bottom: 2px solid #6660A9;
+    padding: 10px;
+    border-bottom: 2px solid #6660A9;
 
 &:hover {
     background-color: rgba(0, 0, 0, 0.2);
